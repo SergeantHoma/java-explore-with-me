0754ac4a7061ac2a9026cfbc95009370d1d01104
Valdimir Esin <vova.esin.00@mail.ru>
2024-12-12 01:46:27 +0300

    second_0.1 commit

--- ewm-stats-service/stats-dto/src/main/java/ru/practicum/StatDtoInput.java
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/StatDtoInput.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,22 +16,23 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class StatDtoInput {
 
     @NotBlank
     @Size(max = 255)
-    private String app;
+    private String app; //Идентификатор сервиса для которого записывается информация
 
     @NotBlank
     @NotEmpty
     @Size(max = 255)
-    private String uri;
+    private String uri; //URI для которого был осуществлен запрос
 
     @NotBlank
     @Size(max = 255)
-    private String ip;
+    private String ip; //IP-адрес пользователя, осуществившего запрос
 
     @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
-    private LocalDateTime timestamp;
+    private LocalDateTime timestamp; //Дата и время, когда был совершен запрос к эндпоинту (в формате "yyyy-MM-dd HH:mm:ss")
 }
